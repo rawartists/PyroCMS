8a4a96b58f3dfd38a0bcfcc5b95f438d9d76e98e
Matthew Klahorst <mklahorst@gmail.com>
2014-05-27 21:27:03 -0700

    Added benchmarking to controller

--- system/cms/core/Admin_Controller.php
+++ system/cms/core/Admin_Controller.php
@@ -24,9 +24,8 @@ class Admin_Controller extends MY_Controller
      */
     public function __construct()
     {
-
+        // Start Benchmarking
         ci()->benchmark->mark('admin_controller_start');
-
 
         ci()->benchmark->mark('admin_controller_construct_my_controller_start');
         parent::__construct();
@@ -52,11 +51,11 @@ class Admin_Controller extends MY_Controller
         }
 
         ci()->benchmark->mark('admin_controller_admin_theme_start');
-        $this->load->helper('admin_theme');
+            $this->load->helper('admin_theme');
         ci()->benchmark->mark('admin_controller_admin_theme_end');
 
         ci()->benchmark->mark('admin_controller_admin_theme_manager_start');
-        $theme = $this->themeManager->locate(Settings::get('admin_theme'));
+            $theme = $this->themeManager->locate(Settings::get('admin_theme'));
         ci()->benchmark->mark('admin_controller_admin_theme_manager_end');
 
         // Using a bad slug? Weak
@@ -85,161 +84,17 @@ class Admin_Controller extends MY_Controller
         Events::trigger('admin_controller');
         ci()->benchmark->mark('admin_controller_trigger_events_end');
 
-        // -------------------------------------
-        // Build Admin Navigation
-        // -------------------------------------
-        // We'll get all of the backend modules
-        // from the DB and run their module items.
-        // -------------------------------------
 
 
+        // Admin Menu
         ci()->benchmark->mark('admin_controller_building_navigation_start');
-
-        // Cache Menu
-        $menu_key = 'my_admin_menu';
-
         if (is_logged_in()) {
-
-            if (!$my_menu = ci()->cache->get($menu_key)) {
-
-                // Here's our menu array.
-                $menu_items = array();
-
-                // This array controls the order of the admin items.
-                $this->template->menu_order = array(
-                    array(
-                        'before' => '<i class="fa fa-book"></i>',
-                        'title'  => 'lang:cp:nav_content',
-                        'items'  => array(),
-                    ),
-                    array(
-                        'before' => '<i class="fa fa-sitemap"></i>',
-                        'title'  => 'lang:cp:nav_structure',
-                        'items'  => array(),
-                    ),
-                    array(
-                        'before' => '<i class="fa fa-hdd-o"></i>',
-                        'title'  => 'lang:cp:nav_data',
-                        'items'  => array(),
-                    ),
-                    array(
-                        'before' => '<i class="fa fa-group"></i>',
-                        'title'  => 'lang:cp:nav_users',
-                        'items'  => array(),
-                    ),
-                );
-
-                $modules = $this->moduleManager->getAllEnabled(
-                    array(
-                        'is_backend' => true,
-                    )
-                );
-
-                foreach ($modules as $module) {
-
-                    // Only enabled ones
-                    if (!module_enabled($module['slug'])) {
-                        continue;
-                    }
-
-                    // If we do not have an admin_menu function, we use the
-                    // regular way of checking out the details.php data.
-                    if ($module['menu'] and ($this->current_user->hasAccess($module['slug'] . '.*'))) {
-
-                        // Legacy module routing. This is just a rough
-                        // re-route and modules should change using their
-                        // upgrade() details.php functions.
-                        if ($module['menu'] == 'utilities') {
-                            $module['menu'] = 'data';
-                        }
-                        if ($module['menu'] == 'design') {
-                            $module['menu'] = 'structure';
-                        }
-
-                        $menu_items['lang:cp:nav_' . $module['menu']][$module['name']] = 'admin/' . $module['slug'];
-                    }
-
-                    // If a module has an admin_menu function, then
-                    // we simply run that and allow it to manipulate the
-                    // menu array.
-                    if (method_exists($module['module'], 'admin_menu')) {
-                        $module['module']->admin_menu($menu_items);
-                    }
-                }
-
-                // Order the menu items. We go by our menu_order array.
-                $ordered_menu = array();
-
-                foreach ($this->template->menu_order as $order) {
-
-                    // We need to follow standards
-                    if (isset($order['title']) and isset($menu_items[$order['title']])) {
-
-                        // Add our menu starter
-                        $ordered_menu[lang_label($order['title'])] = $order;
-
-                        // Do we have items or a URI?
-                        if (is_array($menu_items[$order['title']])) {
-                            $ordered_menu[lang_label($order['title'])]['items'] = $menu_items[$order['title']];
-                        } elseif (is_string($menu_items[$order['title']])) {
-                            $ordered_menu[lang_label($order['title'])]['uri'] = $menu_items[$order['title']];
-                            unset($ordered_menu[lang_label($order['title'])]['items']);
-                        }
-
-                        // Bai
-                        unset($menu_items[$order['title']]);
-                    }
-                }
-
-                // Any stragglers?
-                if ($menu_items) {
-                    $translated_menu_items = array();
-
-                    // translate any additional top level menu keys so the array_merge works
-                    foreach ($menu_items as $key => $menu_item) {
-                        $translated_menu_items[lang_label($key)] = $menu_item;
-                    }
-
-                    $ordered_menu = array_merge_recursive($ordered_menu, $translated_menu_items);
-                }
-
-                ksort($ordered_menu);
-
-                // Trigger an event so modules can mess with the
-                // menu items array via the events structure.
-                $event_output = Events::trigger('admin_menu', $ordered_menu, 'array');
-
-                // If we get an array, we assume they have altered the menu items
-                // and are returning them to us to use.
-                if (isset($event_output[0]) and is_array($event_output[0])) {
-                    $ordered_menu = $event_output[0];
-                }
-
-
-                $my_menu = $ordered_menu;
-
-                // Save to cache
-                ci()->cache->put($menu_key, $ordered_menu, 3000);
-            }
-
-            // And there we go! These are the admin menu items.
-            $this->template->menu_items = $my_menu;
-
-
-            // var_dump(ci()->cache->get($key));
-
-
-        }
+            $this->buildAdminMenu();
         ci()->benchmark->mark('admin_controller_building_navigation_end');
-        //$this->template->menu_items = $my_menu = ci()->cache->get($key);
-        //$this->template->menu_items = array();
+        }
 
-        //var_dump(ci()->cache->get($key));
-
-        // ------------------------------
-
-        ci()->benchmark->mark('admin_controller_template_config_start');
         // Template configuration
+        ci()->benchmark->mark('admin_controller_template_config_start');
         $this->template
             ->enable_parser(false)
             ->set('theme_options', (object)$this->theme->model->getOptionValues())
@@ -247,12 +102,14 @@ class Admin_Controller extends MY_Controller
             ->set_layout('default', 'admin');
         ci()->benchmark->mark('admin_controller_template_config_end');
 
-        ci()->benchmark->mark('admin_controller_run_method_start');
+
         // trigger the run() method in the selected admin theme
+        ci()->benchmark->mark('admin_controller_run_method_start');
         $class = 'Theme_' . ucfirst($this->theme->model->slug);
         call_user_func(array(new $class, 'run'));
         ci()->benchmark->mark('admin_controller_run_method_end');
 
+        // End Benchmarkfor Admin Controller
         ci()->benchmark->mark('admin_controller_end');
     }
 
@@ -318,6 +175,148 @@ class Admin_Controller extends MY_Controller
                 SHARED_ADDONPATH . 'widgets/',
             )
         );
+    }
+
+    protected function buildAdminMenu()
+    {
+
+        // Cached menu key for each user
+        $menu_key = 'my_admin_menu_' . ci()->current_user->id;
+
+        // If not cached menu, build it
+        if (!$my_menu = ci()->cache->get($menu_key)) {
+        //if (1 == 1) {
+
+            // -------------------------------------
+            // Build Admin Navigation
+            // -------------------------------------
+            // We'll get all of the backend modules
+            // from the DB and run their module items.
+            // -------------------------------------
+
+
+            // Here's our menu array.
+            $menu_items = array();
+
+            // This array controls the order of the admin items.
+            $this->template->menu_order = array(
+                array(
+                    'before' => '<i class="fa fa-book"></i>',
+                    'title'  => 'lang:cp:nav_content',
+                    'items'  => array(),
+                ),
+                array(
+                    'before' => '<i class="fa fa-sitemap"></i>',
+                    'title'  => 'lang:cp:nav_structure',
+                    'items'  => array(),
+                ),
+                array(
+                    'before' => '<i class="fa fa-hdd-o"></i>',
+                    'title'  => 'lang:cp:nav_data',
+                    'items'  => array(),
+                ),
+                array(
+                    'before' => '<i class="fa fa-group"></i>',
+                    'title'  => 'lang:cp:nav_users',
+                    'items'  => array(),
+                ),
+            );
+
+            $modules = $this->moduleManager->getAllEnabled(
+                array(
+                    'is_backend' => true,
+                )
+            );
+
+            foreach ($modules as $module) {
+
+                // Only enabled ones
+                if (!module_enabled($module['slug'])) {
+                    continue;
+                }
+
+                // If we do not have an admin_menu function, we use the
+                // regular way of checking out the details.php data.
+                if ($module['menu'] and ($this->current_user->hasAccess($module['slug'] . '.*'))) {
+
+                    // Legacy module routing. This is just a rough
+                    // re-route and modules should change using their
+                    // upgrade() details.php functions.
+                    if ($module['menu'] == 'utilities') {
+                        $module['menu'] = 'data';
+                    }
+                    if ($module['menu'] == 'design') {
+                        $module['menu'] = 'structure';
+                    }
+
+                    $menu_items['lang:cp:nav_' . $module['menu']][$module['name']] = 'admin/' . $module['slug'];
+                }
+
+                // If a module has an admin_menu function, then
+                // we simply run that and allow it to manipulate the
+                // menu array.
+                if (method_exists($module['module'], 'admin_menu')) {
+                    $module['module']->admin_menu($menu_items);
+                }
+            }
+
+            // Order the menu items. We go by our menu_order array.
+            $ordered_menu = array();
+
+            foreach ($this->template->menu_order as $order) {
+
+                // We need to follow standards
+                if (isset($order['title']) and isset($menu_items[$order['title']])) {
+
+                    // Add our menu starter
+                    $ordered_menu[lang_label($order['title'])] = $order;
+
+                    // Do we have items or a URI?
+                    if (is_array($menu_items[$order['title']])) {
+                        $ordered_menu[lang_label($order['title'])]['items'] = $menu_items[$order['title']];
+                    } elseif (is_string($menu_items[$order['title']])) {
+                        $ordered_menu[lang_label($order['title'])]['uri'] = $menu_items[$order['title']];
+                        unset($ordered_menu[lang_label($order['title'])]['items']);
+                    }
+
+                    // Bai
+                    unset($menu_items[$order['title']]);
+                }
+            }
+
+            // Any stragglers?
+            if ($menu_items) {
+                $translated_menu_items = array();
+
+                // translate any additional top level menu keys so the array_merge works
+                foreach ($menu_items as $key => $menu_item) {
+                    $translated_menu_items[lang_label($key)] = $menu_item;
+                }
+
+                $ordered_menu = array_merge_recursive($ordered_menu, $translated_menu_items);
+            }
+
+            ksort($ordered_menu);
+
+            // Trigger an event so modules can mess with the
+            // menu items array via the events structure.
+            $event_output = Events::trigger('admin_menu', $ordered_menu, 'array');
+
+            // If we get an array, we assume they have altered the menu items
+            // and are returning them to us to use.
+            if (isset($event_output[0]) and is_array($event_output[0])) {
+                $ordered_menu = $event_output[0];
+            }
+
+            $my_menu = $ordered_menu;
+
+            // Save to cache
+            ci()->cache->put($menu_key, $ordered_menu, 3000);
+        }
+
+        // And there we go! These are the admin menu items.
+        $this->template->menu_items = $my_menu;
+
     }
 
 }
