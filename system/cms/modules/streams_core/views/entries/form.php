41ee0ca60d1a11c7f055478a82e0b31ce5961093
Matthew Klahorst <mklahorst@gmail.com>
2014-06-04 17:20:45 -0700

    Support for button meta and sending a custom javascript message to be used in the future

--- system/cms/modules/streams_core/views/entries/form.php
+++ system/cms/modules/streams_core/views/entries/form.php
@@ -28,21 +28,21 @@
 
 <?php if (!$disableFormOpen): ?>
 <div class="panel-footer">
-	<button type="submit" name="btnAction" value="save" class="btn btn-success"><?php echo lang('buttons:save'); ?></button>
+	<button type="submit" name="btnAction" value="save" class="btn btn-success <?php echo (array_key_exists('class', $saveButtonMeta)) ? $saveButtonMeta['class'] : ''; ?>" ><?php echo (array_key_exists('name', $saveButtonMeta)) ? $saveButtonMeta['name'] : lang('buttons:save'); ?></button>
 	
 	<?php if (! empty($redirectExit)): ?>
-	<button type="submit" name="btnAction" value="exit" class="btn btn-info"><?php echo lang('buttons:save_exit'); ?></button>
+	<button type="submit" name="btnAction" value="exit" class="btn btn-info <?php echo (array_key_exists('class', $saveExitButtonMeta)) ? $saveExitButtonMeta['class'] : ''; ?>"><?php echo (array_key_exists('name', $saveExitButtonMeta)) ? $saveExitButtonMeta['name'] : lang('buttons:save_exit'); ?></button>
 	<?php endif; ?>
 
 	<?php if (! empty($redirectCreate)): ?>
-	<button type="submit" name="btnAction" value="create" class="btn btn-info"><?php echo lang('buttons:save_create'); ?></button>
+	<button type="submit" name="btnAction" value="create" class="btn btn-info <?php echo (array_key_exists('class', $saveCreateButtonMeta)) ? $saveCreateButtonMeta['class'] : ''; ?>"><?php echo (array_key_exists('name', $saveCreateButtonMeta)) ? $saveCreateButtonMeta['name'] : lang('buttons:save_create'); ?></button>
 	<?php endif; ?>
 
 	<?php if (! empty($redirectContinue)): ?>
-	<button type="submit" name="btnAction" value="continue" class="btn btn-info"><?php echo lang('buttons:save_continue'); ?></button>
+	<button type="submit" name="btnAction" value="continue" class="btn btn-info <?php echo (array_key_exists('class', $saveContinueButtonMeta)) ? $saveContinueButtonMeta['class'] : ''; ?>"><?php echo (array_key_exists('name', $saveContinueButtonMeta)) ? $saveContinueButtonMeta['name'] : lang('buttons:save_continue'); ?></button>
 	<?php endif; ?>
 
-	<a href="<?php echo site_url(isset($uriCancel) ? $uriCancel : $redirectSave); ?>" class="btn btn-default"><?php echo lang('buttons:cancel'); ?></a>		
+	<a href="<?php echo site_url(isset($uriCancel) ? $uriCancel : $redirectSave); ?>" class="btn btn-default <?php echo (array_key_exists('class', $cancelButtonMeta)) ? $cancelButtonMeta['class'] : ''; ?>"><?php echo (array_key_exists('name', $cancelButtonMeta)) ? $cancelButtonMeta['name'] : lang('buttons:cancel'); ?></a>
 </div>
 <?php endif; ?>
 
@@ -65,3 +65,20 @@
 </div>
 
 <?php endif; ?>
+
+<script>
+    // Might use this later to pass a custom message
+    $(function() {
+
+        $('button').click(function() {
+
+            // if have customMessage class, show confirmation
+            //alert('this got clicked, but not returning');
+            // return false;
+
+        })
+
+
+    });
+
+</script>
