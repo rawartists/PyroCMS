22bcdbf9c033d6bf2f0b4f44a6eeaae50cd6513e
Matthew Klahorst <mklahorst@gmail.com>
2014-06-11 15:50:20 -0700

    Moved buttons out to include file

--- system/cms/modules/streams_core/views/entries/form.php
+++ system/cms/modules/streams_core/views/entries/form.php
@@ -27,23 +27,7 @@
 <?php if (!$new) { ?><input type="hidden" value="<?php echo $entry->id;?>" name="row_edit_id" /><?php } ?>
 
 <?php if (!$disableFormOpen): ?>
-<div class="panel-footer">
-	<button type="submit" name="btnAction" value="save" class="btn btn-success <?php echo (array_key_exists('class', $saveButtonMeta)) ? $saveButtonMeta['class'] : ''; ?>" ><?php echo (array_key_exists('name', $saveButtonMeta)) ? $saveButtonMeta['name'] : lang('buttons:save'); ?></button>
-	
-	<?php if (! empty($redirectExit)): ?>
-	<button type="submit" name="btnAction" value="exit" class="btn btn-info <?php echo (array_key_exists('class', $saveExitButtonMeta)) ? $saveExitButtonMeta['class'] : ''; ?>"><?php echo (array_key_exists('name', $saveExitButtonMeta)) ? $saveExitButtonMeta['name'] : lang('buttons:save_exit'); ?></button>
-	<?php endif; ?>
-
-	<?php if (! empty($redirectCreate)): ?>
-	<button type="submit" name="btnAction" value="create" class="btn btn-info <?php echo (array_key_exists('class', $saveCreateButtonMeta)) ? $saveCreateButtonMeta['class'] : ''; ?>"><?php echo (array_key_exists('name', $saveCreateButtonMeta)) ? $saveCreateButtonMeta['name'] : lang('buttons:save_create'); ?></button>
-	<?php endif; ?>
-
-	<?php if (! empty($redirectContinue)): ?>
-	<button type="submit" name="btnAction" value="continue" class="btn btn-info <?php echo (array_key_exists('class', $saveContinueButtonMeta)) ? $saveContinueButtonMeta['class'] : ''; ?>"><?php echo (array_key_exists('name', $saveContinueButtonMeta)) ? $saveContinueButtonMeta['name'] : lang('buttons:save_continue'); ?></button>
-	<?php endif; ?>
-
-	<a href="<?php echo site_url(isset($uriCancel) ? $uriCancel : $redirectSave); ?>" class="btn btn-default <?php echo (array_key_exists('class', $cancelButtonMeta)) ? $cancelButtonMeta['class'] : ''; ?>"><?php echo (array_key_exists('name', $cancelButtonMeta)) ? $cancelButtonMeta['name'] : lang('buttons:cancel'); ?></a>
-</div>
+        <?php include('buttons.php'); ?>
 <?php endif; ?>
 
 
