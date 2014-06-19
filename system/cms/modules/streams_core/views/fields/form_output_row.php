108ef268c5720492ceefca2a996b6d5843aef98b
Ryan Thompson <service@aiwebsystems.com>
2014-06-18 21:19:01 -0500

    Use the presenter method if available.

--- system/cms/modules/streams_core/views/fields/form_output_row.php
+++ system/cms/modules/streams_core/views/fields/form_output_row.php
@@ -1,4 +1,6 @@
-<label class="col-lg-2 control-label" for="<?php echo $field_type->form_slug; ?>">
+<label class="col-lg-2 control-label" for="<?php use Illuminate\Support\Str;
+
+echo $field_type->form_slug; ?>">
     <?php echo $field_type->getField()->field_name; ?>
 
     <?php if ($field_type->getField()->is_required): ?>
@@ -13,5 +15,9 @@
 </label>
 
 <div class="col-lg-10">
+    <?php if (method_exists($field_type->entry->getPresenter(), Str::studly('readOnly_'.$field_type->getField()->field_slug))): ?>
+    <?php echo $field_type->entry->getPresenter()->{Str::studly('readOnly_'.$field_type->getField()->field_slug)}(); ?>
+    <?php else: ?>
     <?php echo $field_type->stringOutput(); ?>
+    <?php endif; ?>
 </div>
