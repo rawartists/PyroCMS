81969edd3f041b5b118f6ed85a819510a0a39eec
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-07-31 13:05:23 -0500

    Update slug form input view

--- system/cms/modules/pages/views/fields/slug.php
+++ system/cms/modules/pages/views/fields/slug.php
@@ -1,14 +1,24 @@
-<label for="<?php echo $field_type->form_slug; ?>"><?php echo lang_label($field_type->getField()->field_name); ?>
+<label class="col-lg-2 control-label" for="<?php
+use Pyro\Module\Pages\Model\Page;
 
-    <?php if ($field_type->getField()->is_required): ?><span class="required">*</span><?php endif; ?>
+echo $field_type->form_slug; ?>"><?php echo lang_label($field_type->getField()->field_name); ?>
+
+    <?php if ($field_type->getField()->is_required): ?>
+        <span class="required">*</span>
+    <?php endif; ?>
 
     <?php if (!empty($field_type->getField()->instructions)): ?>
-        <br/>
-        <small><?php echo lang_label($field_type->getField()->instructions); ?></small>
+    <p class="help-block c-gray-light"><?php echo lang_label($field_type->getField()->instructions); ?></p>
     <?php endif; ?>
 
 </label>
 
-<div class="input">
-    <?php echo site_url(); ?><?php echo $field_type->getInput(); ?>
+<div class="col-lg-10">
+    <div class="d-inline-block">
+        <?php $page = Page::find(ci()->input->get('parent')); ?>
+        <?php echo site_url($page ? $page->uri : null) . ($page ? '/' : null); ?>
+    </div>
+    <div class="d-inline-block">
+        <?php echo $field_type->getInput(); ?>
+    </div>
 </div>
